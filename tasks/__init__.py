ffad3784f6054e678d6080d1e5f722304307343c
edmondfrank <edmondfrank@hotmail.com>
2022-08-23 15:36:34 +0800

    Refactor logging.
    Signed-off-by: edmondfrank <edmondfrank@hotmail.com>

---
+++ tasks/__init__.py
@@ -0,0 +1,40 @@
+import logging
+import colorlog
+
+DEBUG_LOG_FORMAT = "[%(asctime)s - %(name)s - %(levelname)s] - %(message)s"
+INFO_LOG_FORMAT = "%(asctime)s %(message)s"
+COLOR_LOG_FORMAT_SUFFIX = "\033[1m %(log_color)s "
+LOG_COLORS = {'DEBUG': 'white', 'INFO': 'cyan', 'WARNING': 'yellow', 'ERROR': 'red', 'CRITICAL': 'red,bg_white'}
+
+def config_logging(debug, logs_dir, append=True):
+    """Config logging level output output"""
+
+    if debug:
+        fmt = DEBUG_LOG_FORMAT
+        logging_mode = logging.DEBUG
+    else:
+        fmt = INFO_LOG_FORMAT
+        logging_mode = logging.INFO
+
+    # Setting the color scheme and level into the root logger
+    logging.basicConfig(level=logging_mode)
+    stream = logging.root.handlers[0]
+    formatter = colorlog.ColoredFormatter(fmt=COLOR_LOG_FORMAT_SUFFIX + fmt,
+                                          log_colors=LOG_COLORS)
+    stream.setFormatter(formatter)
+
+    # Creating a file handler and adding it to root
+    if logs_dir:
+        fh_filepath = os.path.join(logs_dir, 'all.log')
+        fh = logging.FileHandler(fh_filepath, mode=('a' if append else 'w'))
+        fh.setLevel(logging_mode)
+        formatter = logging.Formatter(fmt)
+        fh.setFormatter(formatter)
+        logging.getLogger().addHandler(fh)
+
+    # ES logger is set to INFO since, it produces a really verbose output if set to DEBUG
+    logging.getLogger('elasticsearch').setLevel(logging.WARNING)
+
+    # Show if debug mode is activated
+    if debug:
+        logging.debug("Debug mode activated")
